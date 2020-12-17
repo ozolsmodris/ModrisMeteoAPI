2d8b983ff70288435552475df15e901015ba8440
Modris Ozols <modris.ozols@testdevlab.com>
2020-12-17 11:38:49 +0200

    Initial Commit

---
+++ ModrisMeteoAPI/ModrisMeteoAPI.h
@@ -0,0 +1,18 @@
+//
+//  ModrisMeteoAPI.h
+//  ModrisMeteoAPI
+//
+//  Created by Modris Ozols on 17/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModrisMeteoAPI.
+FOUNDATION_EXPORT double ModrisMeteoAPIVersionNumber;
+
+//! Project version string for ModrisMeteoAPI.
+FOUNDATION_EXPORT const unsigned char ModrisMeteoAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModrisMeteoAPI/PublicHeader.h>
+
+
