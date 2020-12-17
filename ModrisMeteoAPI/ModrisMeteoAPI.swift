38cd4be0f857193e80556e7931fb56c00f6044fb
Modris Ozols <modris.ozols@testdevlab.com>
2020-12-17 22:40:46 +0200

    ModrisMeteoAPI.swift update

--- ModrisMeteoAPI/ModrisMeteoAPI.swift
+++ ModrisMeteoAPI/ModrisMeteoAPI.swift
@@ -6,35 +6,29 @@
 //
 
 import Foundation
-public class ModrisMeteoAPI {
-    let url = URL(string: "https://www.meteo.lv/meteorologijas-operativie-dati")!
-    public init() {}
-    
-    /// Return temperature
-    /// - Parameter completion: completion returns Quote structure
-    public func fetchTemperatureForLocation(_ location: String, completion: @escaping (Station?, Error?) -> Void) {
-        
-        let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
-              if let error = error {
-                print("Error with fetching films: \(error)")
-                completion(nil, error)
-                return
-              }
-              
-              guard let httpResponse = response as? HTTPURLResponse,
-                    (200...299).contains(httpResponse.statusCode) else {
-                print("Error with the response, unexpected status code: \(String(describing: response))")
-                completion(nil, error)
-                return
-              }
 
-              if let data = data,
-                 let temperatureData = try? JSONDecoder().decode(TemperatureData.self, from: data) {
-                completion(temperatureData.stations.first, nil)               
-              }
-            })
-            task.resume()
+final class ModrisMeteoAPI {
+    let url = URL(string: "https://www.meteo.lv/meteorologijas-operativie-dati")!
+    
+    /// Return all stations
+    /// - Parameter completion: completion returns [Station], Error?
+    public func fetchAllStation( completionHandler: @escaping (Result<[Station], Error>) -> Void ) {
+        
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, _, error) in
+            if let error = error {
+                print("Error with fetching films: \(error)")
+                completionHandler(.failure(error))
+                return
+            }
+                
+            do {
+                guard let data = data else { return }
+                let temperatureData = try JSONDecoder().decode(TemperatureData.self, from: data)
+                completionHandler(.success(temperatureData.stations))
+            } catch let error {
+                completionHandler(.failure(error))
+            }
+            
+        }).resume()
     }
 }
-
-
