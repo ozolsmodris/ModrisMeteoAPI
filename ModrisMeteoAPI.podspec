0eaef748f5d32c9fbf067590f7160c46f50ae73c
Modris Ozols <modris.ozols@testdevlab.com>
2020-12-17 23:09:38 +0200

    pod lib init

--- ModrisMeteoAPI.podspec
+++ ModrisMeteoAPI.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'ModrisMeteoAPI'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of ModrisMeteoAPI.'
+  s.summary          = 'ModrisMeteoAPI is API framework'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,7 +18,7 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+    API framework to retrieve metrology station data
                        DESC
 
   s.homepage         = 'https://github.com/Modris Ozols/ModrisMeteoAPI'
@@ -28,7 +28,7 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/Modris Ozols/ModrisMeteoAPI.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '9.0'
+  s.ios.deployment_target = '13.0'
 
   s.source_files = 'ModrisMeteoAPI/Classes/**/*'
   
---
+++ ModrisMeteoAPI/Classes/ModrisMeteoAPI.swift
@@ -0,0 +1,34 @@
+//
+//  ModrisMeteoAPI.swift
+//  ModrisMeteoAPI
+//
+//  Created by Modris Ozols on 17/12/2020.
+//
+
+import Foundation
+
+final class ModrisMeteoAPI {
+    let url = URL(string: "https://www.meteo.lv/meteorologijas-operativie-dati")!
+    
+    /// Return all stations
+    /// - Parameter completion: completion returns [Station], Error?
+    public func fetchAllStation( completionHandler: @escaping (Result<[Station], Error>) -> Void ) {
+        
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, _, error) in
+            if let error = error {
+                print("Error with fetching films: \(error)")
+                completionHandler(.failure(error))
+                return
+            }
+                
+            do {
+                guard let data = data else { return }
+                let temperatureData = try JSONDecoder().decode(TemperatureData.self, from: data)
+                completionHandler(.success(temperatureData.stations))
+            } catch let error {
+                completionHandler(.failure(error))
+            }
+            
+        }).resume()
+    }
+}
